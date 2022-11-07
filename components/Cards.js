48abce45861e70167634731cad6efaae7c1ee433
CharlieAL <charlie.lerma.ramirez@gmail.com>
2022-11-07 00:14:20 -0700

    usando flashlist y cambiando el design dark

--- components/Cards.js
+++ components/Cards.js
@@ -1,36 +1,65 @@
-import { View, Text, Image, TouchableOpacity } from 'react-native'
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native'
 import React from 'react'
-import { MapIcon, MapPinIcon, UserIcon } from 'react-native-heroicons/solid'
+import { MapIcon, UserIcon } from 'react-native-heroicons/solid'
 
-const Cards = ({ img, description, sold, max, name, date }) => {
+const Cards = ({
+  img,
+  description,
+  sold,
+  max,
+  name,
+  date,
+  nameOfEvent,
+  hour
+}) => {
   return (
-    <View className='-m-3 -mt-5'>
-      <View>
+    <View className='py-3 '>
+      {/* <View className='absolute top-0 bg-black'></View> */}
+      <View className='flex-row justify-between py-3 px-2 border border-b-gray-800'>
+        <Text className='text-white text-sm font-semibold'>{nameOfEvent}</Text>
+        <Text className='text-white text-sm font-semibold'>
+          {date} a las {hour}
+        </Text>
+      </View>
+      <TouchableOpacity
+        onPress={() => {
+          console.log('click')
+        }}
+        className='bg-black'
+      >
         <Image
+          style={styles.image}
           source={{
             uri: img
           }}
-          className='w-full h-[500px] rounded-t-xl object-cover '
+          resizeMode='stretch'
         />
-      </View>
-      <View className='bg-gray-100 shadow-lg rounded-b-xl h-44'>
-        <View className='flex-row items-center p-3  justify-between'>
-          <TouchableOpacity className='flex-row items-center'>
-            <UserIcon color={'green'} />
-            <Text className='text-lg'>: {sold}</Text>
+      </TouchableOpacity>
+
+      <View className='bg-black shadow-lg h-32 px-1 pt-2'>
+        <View className='flex-row items-center px-2 justify-between'>
+          <TouchableOpacity className='px-2 py-1 rounded-lg flex-row items-center space-x-1 border border-gray-700'>
+            <UserIcon size={25} color={'green'} />
+            <Text className='text-white'>{sold} -</Text>
+            <Text className='text-white'>{max}</Text>
           </TouchableOpacity>
-          <TouchableOpacity className='flex-row items-center'>
-            <MapPinIcon color={'red'} />
-            <Text className='text-lg'>Ver Mapa</Text>
+          <TouchableOpacity className='px-2 py-1 rounded-lg flex-row items-center space-x-1 border border-gray-700'>
+            <MapIcon size={25} color={'red'} />
+            <Text className='text-white'>Ver Mapa</Text>
           </TouchableOpacity>
         </View>
-        <View className='px-2 py-3'>
-          <Text>Fecha: {date}</Text>
-          <Text className='text-lg font-extralight'>{description}</Text>
-        </View>
+        <Text className='text-base font-light text-white '>{description} </Text>
       </View>
     </View>
   )
 }
 
 export default Cards
+
+const styles = StyleSheet.create({
+  image: {
+    width: '100%',
+    height: 450,
+    backgroundColor: 'black'
+  }
+})
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,158 +1,156 @@
-import {
-  View,
-  Text,
-  SafeAreaView,
-  TouchableOpacity,
-  Image,
-  Button
-} from 'react-native'
-import React, { useEffect, useState } from 'react'
-import {
-  ChevronDownIcon,
-  MapPinIcon,
-  ChatBubbleOvalLeftEllipsisIcon,
-  XCircleIcon,
-  XMarkIcon,
-  UserIcon
-} from 'react-native-heroicons/solid'
+import { View, Text, StatusBar, Dimensions, StyleSheet } from 'react-native'
+
 import Cards from '../components/Cards'
 import Navbar from '../components/Navbar'
-import { useNavigation } from '@react-navigation/native'
-import Swiper from 'react-native-deck-swiper'
+
+import useAuth from '../hooks/useAuth'
+import { FlashList } from '@shopify/flash-list'
+import Header from '../components/Header'
 
 const DUMMY_DATA = [
   {
     id: 1,
     name: 'Roberto Perez',
+    nameOfEvent: 'Fiesta de la cerveza',
     image:
       'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/hip-hop-rap-music-event-flyer-template-design-3b828da63aad5f3e4b3388af9d6c9b9c_screen.jpg?ts=1636985494',
-    ticketsMax: 100,
-    ticketsSold: 50,
+    ticketsMax: 400,
+    ticketsSold: 73,
     descripction:
       '1 Batallas de Rap en vivo en el centro de la ciudad de Mexico mejor evento de rap en vivo',
-    date: '2022-11-11'
+    date: '2022-11-11',
+    hour: '22:00',
+    location: 'Mexico'
   },
   {
     id: 2,
     name: 'Charlie Martinez',
+    nameOfEvent: 'Fiesta de la cerveza',
     image:
       'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/techno-party-flyer-template-design-98b3f955ea7ac8622799eee7f6bb5adc_screen.jpg?ts=1636993025',
     ticketsMax: 100,
     ticketsSold: 45,
     descripction:
       '2 La mejor tecno banda en vivo aparten lugar rapido porque se agotan los boletos',
-    date: '2022-11-20'
+    date: '2022-11-20',
+    hour: '22:00',
+    location: 'Mexico'
   },
   {
-    id: 1,
+    id: 3,
     name: 'Roberto Perez',
+    nameOfEvent: 'Fiesta de la cerveza',
     image:
       'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/hip-hop-rap-music-event-flyer-template-design-3b828da63aad5f3e4b3388af9d6c9b9c_screen.jpg?ts=1636985494',
     ticketsMax: 100,
     ticketsSold: 50,
     descripction:
       '3 Batallas de Rap en vivo en el centro de la ciudad de Mexico mejor evento de rap en vivo',
-    date: '2022-11-11'
+    date: '2022-11-11',
+    hour: '22:00',
+    location: 'Mexico'
   },
   {
-    id: 2,
+    id: 4,
     name: 'Charlie Martinez',
+    nameOfEvent: 'Fiesta de la cerveza',
     image:
       'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/techno-party-flyer-template-design-98b3f955ea7ac8622799eee7f6bb5adc_screen.jpg?ts=1636993025',
     ticketsMax: 100,
     ticketsSold: 45,
     descripction:
       '4 La mejor tecno banda en vivo aparten lugar rapido porque se agotan los boletos',
-    date: '2022-11-20'
+    date: '2022-11-20',
+    hour: '22:00',
+    location: 'Mexico'
   },
   {
-    id: 1,
+    id: 5,
     name: 'Roberto Perez',
+    nameOfEvent: 'Fiesta de la cerveza',
     image:
       'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/hip-hop-rap-music-event-flyer-template-design-3b828da63aad5f3e4b3388af9d6c9b9c_screen.jpg?ts=1636985494',
     ticketsMax: 100,
     ticketsSold: 50,
     descripction:
       '5 Batallas de Rap en vivo en el centro de la ciudad de Mexico mejor evento de rap en vivo',
-    date: '2022-11-11'
+    date: '2022-11-11',
+    hour: '22:00',
+    location: 'Mexico'
   },
   {
-    id: 2,
+    id: 6,
     name: 'Charlie Martinez',
+    nameOfEvent: 'Fiesta de la cerveza',
     image:
       'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/techno-party-flyer-template-design-98b3f955ea7ac8622799eee7f6bb5adc_screen.jpg?ts=1636993025',
     ticketsMax: 100,
     ticketsSold: 45,
     descripction:
       '6 La mejor tecno banda en vivo aparten lugar rapido porque se agotan los boletos',
-    date: '2022-11-20'
+    date: '2022-11-20',
+    hour: '22:00',
+    location: 'Mexico'
   },
   {
-    id: 1,
+    id: 7,
     name: 'Roberto Perez',
+    nameOfEvent: 'Fiesta de la cerveza',
     image:
       'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/hip-hop-rap-music-event-flyer-template-design-3b828da63aad5f3e4b3388af9d6c9b9c_screen.jpg?ts=1636985494',
     ticketsMax: 100,
     ticketsSold: 50,
     descripction:
-      '7 Batallas de Rap en vivo en el centro de la ciudad de Mexico mejor evento de rap en vivo',
-    date: '2022-11-11'
+      '7 Batallas de Rap en vivo en el centro de la ciudad de Mexico mejor evento de rap en vivo aksldnlakdnk alsdnlasndlka lkadnlansd',
+    date: '2022-11-11',
+    hour: '22:00',
+    location: 'Mexico'
   }
 ]
 
 const HomeScreen = () => {
-  const navigation = useNavigation()
-
+  const { user } = useAuth()
   return (
-    <SafeAreaView className='flex-1'>
-      {/* Header */}
-      <View className='flex-row items-center justify-between px-5 mt-5'>
-        <TouchableOpacity className=''>
-          <Image
-            source={{
-              uri: 'https://static.vinted.com/assets/no-photo/user-c5c77aad23636edb563b852ff3605caee437bc5d92a2e2e595c209c18b12cffe.png'
-            }}
-            className='w-10 h-10 bg-gray-300 rounded-full p-1 border-2 border-red-400'
+    <>
+      <Header />
+      <View className='flex-1 bg-black'>
+        {/* header */}
+        {/* end header */}
+        {/* Cards */}
+        <View style={styles.cardContainer}>
+          <FlashList
+            data={DUMMY_DATA}
+            estimatedItemSize={200}
+            contentContainerStyle={{}}
+            showsVerticalScrollIndicator={false}
+            renderItem={({ item }) => (
+              <Cards
+                key={item.id}
+                img={item.image}
+                description={item.descripction}
+                sold={item.ticketsSold}
+                max={item.ticketsMax}
+                name={item.name}
+                nameOfEvent={item.nameOfEvent}
+                date={item.date}
+                hour={item.hour}
+              />
+            )}
           />
-        </TouchableOpacity>
-        <TouchableOpacity className='flex-row space-x-1 items-center '>
-          <MapPinIcon size={22} color={'red'} />
-          <Text className='text-lg font-light text-center'>Ubicacion</Text>
-          <ChevronDownIcon size={15} color={'black'} />
-        </TouchableOpacity>
-        <TouchableOpacity
-          className=''
-          onPress={() => {
-            navigation.navigate('Chat')
-          }}
-        >
-          <ChatBubbleOvalLeftEllipsisIcon color={'red'} size={36} />
-        </TouchableOpacity>
+        </View>
+        {/* End of Cards */}
       </View>
-      {/* End of Header */}
-
-      {/* Cards */}
-      <View>
-        <Swiper
-          cards={DUMMY_DATA}
-          renderCard={(card) => (
-            <Cards
-              img={card.image}
-              description={card.descripction}
-              sold={card.ticketsSold}
-              max={card.ticketsMax}
-              name={card.name}
-              date={card.date}
-            />
-          )}
-          stackSize={3}
-          goBackToPreviousCardOnSwipeLeft={true}
-          disableRightSwipe={true}
-        ></Swiper>
-      </View>
-      {/* End of Cards */}
-    </SafeAreaView>
+      {/* NavBottom */}
+      <Navbar user={user} />
+      {/* End of NavBottom */}
+    </>
   )
 }
 
 export default HomeScreen
+
+const styles = StyleSheet.create({
+  cardContainer: {
+    height: Dimensions.get('window').height - 160
+  }
+})
